ea9f850fce46bd0df1f79688f72f15de4f36ea7a
jabyn <1152733497@qq.com>
2022-07-21 20:16:58 +0800

    add checkout

---
+++ src/org/devops/checkout.groovy
@@ -0,0 +1,34 @@
+package org.devops
+
+def Dotnet6CheckOut(PathOfDir, GitName, GitPasswd, BranchName, ScoUrl, GerritRefsm) {
+    def CodeDir = new File("${PathOfDir}")
+    Boolean bool = CodeDir.isDirctory()
+    if (bool) {
+        retry('9999') {
+            dir(path: '') {
+            def cmd = "git fetch --all".execute()
+            cmd.waitForOrKill()
+            }
+        }        
+    } else {
+        mkdir(dir:"${PathOfDir}")
+        dir(path: '${PathOfDir}'){
+            warnError(message: 'origin already exists') {
+                sh """
+                git remote add origin http://${GitName}:${GitPasswd}@rd.loongson.cn:8081/a/mono
+                """
+            }
+            retry('9999') {
+                //checkout([$class: 'GitSCM', branches: [[name: '*/loongarch64-dev-6.0']], extensions: [], userRemoteConfigs: [[credentialsId: 'gerrit', url: 'http://rd.loongson.cn:8081/a/mono']]])
+                checkout([$class: 'GitSCM', branches: [[name: '*/${BranchName}']], extensions: [], userRemoteConfigs: [[credentialsId: 'gerrit', url: '${ScoUrl}']]])
+            }
+        }
+    }
+    dir(path: '${PathOfDir}') {
+        retry('9999') {
+            sh 'git pull origin ${GerritRefs}'
+        }
+    }
+
+}
+
